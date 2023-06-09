027aac31bc32f65ec0259af8ba73661bb62c860a
uday <uday.p@simformsolutions.com>
2023-06-09 12:46:59 +0530

    ts file with added type checks

--- sci-calc/src/app.ts
+++ sci-calc/src/app.ts
@@ -47,70 +47,70 @@ buttons.forEach((button) => {
                 display.textContent! += "%";
                 break;
             case "In":
-                display.textContent! = displayAns(Math.log(+display.textContent!));
+                display.textContent! = displayAns(Math.log(eval(display.textContent!)));
                 break;
             case "log":
-                display.textContent! = displayAns(Math.log10(+display.textContent!));
+                display.textContent! = displayAns(Math.log10(eval(display.textContent!)));
                 break;
             case "π":
                 displayPI(display.textContent!);
                 break;
             case "10x":
-                display.textContent! = displayAns(Math.pow(10, +display.textContent!));
+                display.textContent! = displayAns(Math.pow(10, eval(display.textContent!)));
                 break;
             case "√":
-                display.textContent! = displayAns(Math.sqrt(+display.textContent!));
+                display.textContent! = displayAns(Math.sqrt(eval(display.textContent!)));
                 break;
             case "n!":
                 fact(display.textContent!);
                 break;
             case "x2":
-                display.textContent! = displayAns(Math.pow(+display.textContent!, 2));
+                display.textContent! = displayAns(Math.pow(eval(display.textContent!), 2));
                 break;
             case "1/x":
                 display.textContent! = displayAns(
-                    eval(String(1 / +display.textContent!))
+                    eval(String(1 / eval(display.textContent!)))
                 );
                 break;
             case "|x|":
-                display.textContent! = displayAns(Math.abs(+display.textContent!));
+                display.textContent! = displayAns(Math.abs(eval(display.textContent!)));
                 break;
             case "exp":
             case "e":
-                display.textContent! = displayAns(Math.exp(+display.textContent!));
+                display.textContent! = displayAns(Math.exp(eval(display.textContent!)));
                 break;
             case "sin":
-                display.textContent! = displayAns(Math.sin(+display.textContent!));
+                display.textContent! = displayAns(Math.sin(eval(display.textContent!)));
                 break;
             case "cos":
-                display.textContent! = displayAns(Math.cos(+display.textContent!));
+                display.textContent! = displayAns(Math.cos(eval(display.textContent!)));
                 break;
             case "tan":
-                display.textContent! = displayAns(Math.tan(+display.textContent!));
+                display.textContent! = displayAns(Math.tan(eval(display.textContent!)));
                 break;
             case "asin":
-                display.textContent! = displayAns(Math.asin(+display.textContent!));
+                display.textContent! = displayAns(Math.asin(eval(display.textContent!)));
                 break;
             case "acos":
-                display.textContent! = displayAns(Math.acos(+display.textContent!));
+                display.textContent! = displayAns(Math.acos(eval(display.textContent!)));
                 break;
             case "atan":
-                display.textContent! = displayAns(Math.atan(+display.textContent!));
+                display.textContent! = displayAns(Math.atan(eval(display.textContent!)));
                 break;
             case "⌈x⌉":
-                display.textContent! = displayAns(Math.ceil(+display.textContent!));
+                display.textContent! = displayAns(Math.ceil(eval(display.textContent!)));
                 break;
             case "⌊x⌋":
-                display.textContent! = displayAns(Math.floor(+display.textContent!));
+                display.textContent! = displayAns(Math.floor(eval(display.textContent!)));
                 break;
             case "RAN":
                 display.textContent! = displayAns(Math.random());
                 break;
             case "M+":
-                mAdd(display.textContent!);
+                mAdd(eval(display.textContent!));
                 break;
             case "M-":
-                mSub(display.textContent!);
+                mSub(eval(display.textContent!));
                 break;
             case "MC":
                 mClear();
@@ -119,32 +119,32 @@ buttons.forEach((button) => {
                 mRecall();
                 break;
             case "MS":
-                mStore(display.textContent!);
+                mStore(eval(display.textContent!));
                 break;
             case "DEG":
                 button.textContent = "RAD";
-                display.textContent! = String((+display.textContent! * 180) / Math.PI);
+                display.textContent! = String((eval(display.textContent!) * 180) / Math.PI);
                 break;
             case "RAD":
                 button.textContent = "DEG";
-                display.textContent! = String((Math.PI * +display.textContent!) / 180);
+                display.textContent! = String((Math.PI * eval(display.textContent!)) / 180);
                 break;
             case "F-E":
-                display.textContent! = Number(display.textContent!).toExponential();
+                display.textContent! = eval(display.textContent!).toExponential();
                 break;
             case "2x":
-                display.textContent! = displayAns(Math.pow(2, +display.textContent!));
+                display.textContent! = displayAns(Math.pow(2, eval(display.textContent!)));
                 break;
             case "3x":
-                display.textContent! = displayAns(Math.pow(3, +display.textContent!));
+                display.textContent! = displayAns(Math.pow(3, eval(display.textContent!)));
                 break;
             case "∛x":
                 display.textContent! = displayAns(
-                    Math.pow(+display.textContent!, 1 / 3)
+                    Math.pow(eval(display.textContent!), 1 / 3)
                 );
                 break;
             case "x3":
-                display.textContent! = displayAns(Math.pow(+display.textContent!, 3));
+                display.textContent! = displayAns(Math.pow(eval(display.textContent!), 3));
                 break;
             default:
                 if (display.textContent! == "0" && value != ".") {
@@ -162,7 +162,7 @@ const equals: scientificCalc<string, void> = (Content) => {
         if (Content.length === 2 || Content.includes("^^")) {
             display.textContent! = "Invalid input format!!";
         } else {
-            const lastIndex = Content.lastIndexOf('^');
+            const lastIndex:number = Content.lastIndexOf('^');
             if (!Content.charAt(lastIndex + 1)) {
                 display.textContent! = "Invalid input format!!";
             } else {
@@ -171,7 +171,7 @@ const equals: scientificCalc<string, void> = (Content) => {
             }
         }
     } else if (Content.includes("%")) {
-        const contentLength = Content.length;
+        const contentLength: number = Content.length;
         if (contentLength == 2) {
             display.textContent! = "Invalid input format!!";
         } else {
@@ -179,9 +179,9 @@ const equals: scientificCalc<string, void> = (Content) => {
             if (count > 1) {
                 display.textContent! = "Invalid input format!!";
             } else {
-                const n1: number = Number(Content.slice(0, Content.indexOf('%')));
-                const n2: number = Number(Content.slice(Content.indexOf('%') + 1));
-                if (n2) {
+                const n1: number = eval(Content.slice(0, Content.indexOf('%')));
+                const n2: number = eval(Content.slice(Content.indexOf('%') + 1));
+                if (!n2) {
                     display.textContent! = "Invalid input format!!";
                 } display.textContent! = displayAns(n1 % n2);
             }
@@ -190,7 +190,7 @@ const equals: scientificCalc<string, void> = (Content) => {
         if (Content.length === 2 || Content.includes("//")) {
             display.textContent! = "Invalid input format!!";
         } else {
-            const lastIndex = Content.lastIndexOf('/');
+            const lastIndex:number = Content.lastIndexOf('/');
             if (!Content.charAt(lastIndex + 1)) {
                 display.textContent! = "Invalid input format!!";
             } else {
